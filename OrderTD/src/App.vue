e6fbcedf7d08ed50ca2f6904e49828d4b23b3cdd
E-musik <emmamusik888@gmail.com>
2023-07-20 13:06:58 +0100

    first commit

---
+++ OrderTD/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+
+let home = ('')
+
+const todolist = ref (['']) 
+</script>
+
+<template>
+  <div class="input">
+    <h2>To Do</h2>
+    <input type="text"  v-model="home"
+    placeholder="what todo">
+    <button class="add" @click="todolist.push(home)">Add todo</button>
+    <ol>
+      <li v-for="items of todolist">{{ items }}</li>
+    </ol>
+  </div>
+
+
+</template>
+
+<style scoped>
+
+
+
+</style>
